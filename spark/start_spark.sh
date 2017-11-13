04d232e0ef949b67dc6edf6bcab49524eef06c1c
dingwei-2017 <dingwei2@huawei.com>
2017-11-13 23:16:32 +0800

    Modify dockerfile and add a start_spark script to gurantee docker image not exit

---
+++ spark/start_spark.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+
+###start spark master process
+/usr/local/spark/spark-2.2.0-bin-hadoop2.7/sbin/start-master.sh
+
+#set j=2
+#while true
+#do
+# let "j=j+1"
+#done
+tail -f /dev/null
